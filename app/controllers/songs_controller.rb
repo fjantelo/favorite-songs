7fb85c02c5cd0dc9f06cf83d37fb21d6ef78fca9
Fernando Antelo <fernando@Fernandos-MacBook-Pro-2.local>
2022-07-05 12:47:29 -0400

    Adds delete route and controller method, and requests.http

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,5 +6,12 @@ class SongsController < ApplicationController
 
   def show
     song = Song.find_by(id: params["id"])
+    render json: song.as_json
+  end
+
+  def destroy
+    song = Song.find_by(id: params["id"])
+    song.delete
+    render json: { message: "Song was deleted." }
   end
 end
