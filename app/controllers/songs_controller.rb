864abb7503ebf111dff3f14722f0504d3beadce9
Fernando Antelo <fernando@Fernandos-MacBook-Pro-2.local>
2022-07-05 12:29:16 -0400

    Adds routes and controller methods for index and show methods

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,10 @@
 class SongsController < ApplicationController
+  def index
+    songs = Song.all
+    render json: songs.as_json
+  end
+
+  def show
+    song = Song.find_by(id: params["id"])
+  end
 end
