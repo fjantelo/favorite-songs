636619c4f6153b441a3d1c2bcf0d5180e541e575
Fernando Antelo <fernando@Fernandos-MacBook-Pro-2.local>
2022-07-05 13:26:45 -0400

    Adds patch route, controller method, and http request

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -16,12 +16,23 @@ class SongsController < ApplicationController
   end
 
   def create
-    song = Song.create(
+    song = Song.new(
       title: params["title"],
       album: params["album"],
       artist: params["artist"],
       year: params["year"],
     )
+    song.save
+    render json: song.as_json
+  end
+
+  def update
+    song = Song.find_by(id: params["id"])
+    song.title = params["title"] || song.title
+    song.album = params["album"] || song.album
+    song.artist = params["artist"] || song.artist
+    song.year = params["year"] || song.year
+    song.save
     render json: song.as_json
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get "/songs/:id" => "songs#show"
   delete "/songs/:id" => "songs#destroy"
   post "/songs" => "songs#create"
+  patch "songs/:id" => "songs#update"
 end
